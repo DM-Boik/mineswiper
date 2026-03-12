5f16cba40c9697a2d0ede16fbcdefda2a7d6207e
Anton <qwer@qwe.ru>
2026-03-12 19:53:48 +0300

    тестовые enter

--- mineswiper/mineswiper.cpp
+++ mineswiper/mineswiper.cpp
@@ -1,7 +1,9 @@
 ﻿#include<iostream>
 #include<vector>
 #include <cstdlib>
+
 /*надо сделать выведение всего пустого пространства при его большом количестве*/
+
 using namespace std;
 
 bool blast = false;
@@ -103,6 +105,7 @@ void print() {
     }
     cout << endl;
 }
+
 
 void game_process() {
     int x, y;
